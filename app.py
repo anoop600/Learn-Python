75cbfe9842b6f95d31224989e51c6949095ca6c5
Anoop NB <M1049314@mindtree.com>
2019-08-06 18:32:57 +0530

    updated files

--- app.py
+++ app.py
@@ -1,31 +1,15 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api
 from add import Add
 from subtract import Subtract
 from multiply import Multiply
 from divide import Divide
-from pymongo import MongoClient
+from visit_count import Visit
 
 # Initialize FLASK app
 app = Flask(__name__)
 # Initialize RestFul API for the flask App
 api = Api(app)
-client = MongoClient("mongodb://db:27017")
-
-db = client.aNewDB
-user_num = db["user_num"]
-
-user_num.insert_one({
-    'count': 0
-})
-
-
-class Visit(Resource):
-    def get(self):
-        prev_num = user_num.find({})[0]['count']
-        new_num = prev_num + 1
-        user_num.update_one({}, {"$set": {"count": new_num}})
-        return str("hello user " + str(new_num))
 
 
 # REST-API-Controller
