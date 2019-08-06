898d6a9b4acf850f2fa92abc69072ad4641c8cc5
Anoop NB <M1049314@mindtree.com>
2019-08-06 15:48:39 +0530

    updated db

--- user_count.py
+++ user_count.py
@@ -1,18 +1,19 @@
 from pymongo import MongoClient
 from flask_restful import Resource
+
 client = MongoClient("mongodb://db:27017")
 
 db = client.aNewDB
 user_num = db["user_num"]
 
 user_num.insert({
-    'number_of_users': 0
+    'count': 0
 })
 
 
 class Visit(Resource):
     def get(self):
-        prev_num = user_num.find({})[0]['number of users']
+        prev_num = user_num.find({})[0]['count']
         new_num = prev_num + 1
-        user_num.update({}, {"$set": {"number of users": new_num}})
+        user_num.update({}, {"$set": {"count": new_num}})
         return str("hello user " + str(new_num))
