a000b620d4e85d2e82410b7c199099aa5e73f421
Anoop NB <M1049314@mindtree.com>
2019-08-07 11:31:02 +0530

    added api

--- application-2/app.py
+++ application-2/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from register import Register
 from store import Store
+from get_resource import Get
 
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ api = Api(app)
 
 api.add_resource(Register, "/register")
 api.add_resource(Store, "/store")
+api.add_resource(Get,"/getsentence")
 
 
 if __name__ == "__main__":
