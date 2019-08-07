4d84a9b1995beb7059587bd6f377d3c6e6d79410
Anoop NB <M1049314@mindtree.com>
2019-08-07 18:55:28 +0530

    hotfix

--- application-1/helper.py
+++ application-1/helper.py
@@ -15,26 +15,30 @@ def check_present_in_posted_data(posted_data):
 
 
 def validate_for_divide(posted_data):
-    if check_present_in_posted_data == True:
-                return 301
-            elif int(posted_data["y"]) == 0:
-                return 302
-            else:
-                return 200
+    if check_present_in_posted_data(posted_data) == True:
+        return 301
+    elif int(posted_data["y"]) == 0:
+        return 302
+    else:
+        return 200
+
 
 def validate_for_other(posted_data):
-    if check_present_in_posted_data == True:
-                return 301  # missing parameter
-            else:
-                return 200  # All OK
+    if check_present_in_posted_data(posted_data) == True:
+        return 301  # missing parameter
+    else:
+        return 200  # All OK
+
+
 def validate_posted_data_type(posted_data):
-    return isinstance(posted_data["x"],int) and isinstance(posted_data["y"],int)
+    return isinstance(posted_data["x"], int) and isinstance(posted_data["y"], int)
+
 
 def check_posted_data(posted_data, method):
     if validate_posted_data_type(posted_data) == True
-        if (method == "divide"): 
+       if (method == "divide"):
             return validate_for_divide(posted_data)
-        if(method == "add" or method == "subtract" or method == "multiply"):
+        if (method == "add" or method == "subtract" or method == "multiply"):
             return validate_for_other(posted_data)
     else:
-        return 303 #Type mismatch required int
+        return 303  # Type mismatch required int
