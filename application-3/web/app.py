cfcce1139e6c3dd03817b5d2ab2768c7ce99ec10
Anoop NB <M1049314@mindtree.com>
2019-08-07 16:41:39 +0530

    updated app.py

--- application-3/web/app.py
+++ application-3/web/app.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_restful import Api
+from register import Register
+from detect import Detect
+from refill import Refill
 
 app = Flask(__name__)
 api = Api(app)
 
 
 api.add_resource(Register, "/register")
-api.add_resource(Detect,"/detect")
-api.add_resource(Refill,"/refill")
+api.add_resource(Detect, "/detect")
+api.add_resource(Refill, "/refill")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
