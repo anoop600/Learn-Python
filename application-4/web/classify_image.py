fb15fdd3c9ca1b44f7b4b29f5bb328adcefef56f
Anoop NB <M1049314@mindtree.com>
2019-08-08 17:50:09 +0530

    score converted to string

--- application-4/web/classify_image.py
+++ application-4/web/classify_image.py
@@ -164,7 +164,7 @@ def run_inference_on_image(image):
     for node_id in top_k:
       human_string = node_lookup.id_to_string(node_id)
       score = predictions[node_id]
-      return_json[human_string] = score
+      return_json[human_string] = str(score)
       print('%s (score = %.5f)' % (human_string, score))
     print(return_json)
     with open("text.txt","w") as f:
