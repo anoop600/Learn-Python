4c9615d75bc6ee80d592a51b2547d4564f5334db
Anoop NB <M1049314@mindtree.com>
2019-08-08 16:57:51 +0530

    added dictionary to  store data

--- application-4/web/classify_image.py
+++ application-4/web/classify_image.py
@@ -159,15 +159,16 @@ def run_inference_on_image(image):
 
     # Creates node ID --> English string lookup.
     node_lookup = NodeLookup()
-    print("before top_k")
+    return_json = dict()
     top_k = predictions.argsort()[-FLAGS.num_top_predictions:][::-1]
     for node_id in top_k:
       human_string = node_lookup.id_to_string(node_id)
       score = predictions[node_id]
-      #return_json[human_string] = score
+      return_json[human_string] = score
       print('%s (score = %.5f)' % (human_string, score))
-    #with open("text.txt","w") as f:
-    #  json.dump(return_json,f)
+    print(return_json)
+    with open("text.txt","w") as f:
+      json.dump(return_json,f)
 
 
 def maybe_download_and_extract():
